6b21b11f6b397c57734df49999bd59cd022042d9
Natasha Poon <153544879+n2poon@users.noreply.github.com>
2024-01-03 20:49:33 -0500

    Add files via upload

---
+++ LogicScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    
+    public int playerScore=0;
+    public Text scoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("INCREASE SCORE")]
+    public void addScore()
+    {
+        playerScore += 1;
+        scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
+    }
+}
+
+
